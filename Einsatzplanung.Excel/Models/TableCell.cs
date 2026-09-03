aac65e6a5d78fe5a5dda1348e2d21350d494ba2f
alex <alex.25.lehnert@gmail.com>
2026-09-03 10:43:38 +0200

    Added TableCelll and Table. Cleaned up Einsatzplanung.Excel project file. Cleaned up Einsatzplanung.GUI project file.

---
+++ Einsatzplanung.Excel/Models/TableCell.cs
@@ -0,0 +1,11 @@
+﻿namespace Einsatzplanung.Excel.Models;
+
+using Avalonia.Media;
+
+public class TableCell {
+	
+	public Color BackgroundColor { get; set; } = Colors.White;
+
+	public string Value { get; set; } = "";
+
+}
