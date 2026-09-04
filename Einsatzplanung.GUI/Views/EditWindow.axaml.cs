52493a0b4a2c2276eb112dd5fc9d22a240b50ee1
GitSchwan <fabianschwanck@hotmail.com>
2026-09-04 10:18:04 +0200

    Add `EditWindow` view and code-behind files
    - Introduced `EditWindow.axaml` and `EditWindow.axaml.cs` to define a new user control.

---
+++ Einsatzplanung.GUI/Views/EditWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Einsatzplanung.GUI.Views {
+	public partial class EditWindow : UserControl {
+		public EditWindow() {
+			InitializeComponent();
+		}
+	}
+}
