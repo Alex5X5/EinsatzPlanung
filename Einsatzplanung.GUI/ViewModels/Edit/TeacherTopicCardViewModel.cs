add66af3191b861cf973b18a965fba918de92a6f
MrRuheeeee <mathishenkel@gmail.com>
2026-09-17 09:39:47 +0200

    Add TeacherTopicCardViewModel and bind topics to TeacherPage

---
+++ Einsatzplanung.GUI/ViewModels/Edit/TeacherTopicCardViewModel.cs
@@ -0,0 +1,10 @@
+namespace Einsatzplanung.GUI.ViewModels.Edit {
+    public class TeacherTopicCardViewModel {
+        public string Topic { get; }
+        public TeacherTopicCardViewModel (string topic)
+        {
+            Topic = topic;
+        }
+    
+    }
+}
